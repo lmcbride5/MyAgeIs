8bfbe4c6a1d4632a3e400901346cd7a2b084793a
lmcbride5 <lilibeth.mcbride@patelhs.org>
2020-09-24 22:40:26 +0000

    I made a conditional and an if/else statement comparing my birthday and todays date.

--- Main.java
+++ Main.java
@@ -3,5 +3,11 @@ class Main {
     int myAge = 15;
     int myBirthday = 1125;
     int todaysDate = 921;
+    if (myBirthday == todaysDate)
+    {
+      System.out.println("I am" + myAge + "years old.");
+    }else{
+      System.out.println("I am stil " + myAge + " years old");
+    }
   }
 }
