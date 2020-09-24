6a42287d7234b9094ca2fc7ff86ff2e04286b50a
lmcbride5 <lilibeth.mcbride@patelhs.org>
2020-09-24 22:25:11 +0000

    Created the first age conditional and if/else statement

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    if(myAge == 15)
+    {
+      System.out.println("You are 15 years old.");
+    }else{
+      System.out.println("You are not 15 years old.");
+    }
   }
 }
