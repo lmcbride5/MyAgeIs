c144ecb9ac731d6d809516598bfc4c2eb5bb0864
lmcbride5 <lilibeth.mcbride@patelhs.org>
2020-09-24 22:33:47 +0000

    I re-declared all of my variables.

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 class Main {
   public static void main(String[] args) {
     int myAge = 15;
-    if(myAge == 15)
-    {
-      System.out.println("You are 15 years old.");
-    }else{
-      System.out.println("You are not 15 years old.");
-    }
+    int myBirthday = 1125;
+    int todaysDate = 921;
   }
 }
